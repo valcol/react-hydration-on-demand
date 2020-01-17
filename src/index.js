5907a3c2dec9f3a09316d8819614f99dee98bafa
Valcol <valcol.nc@gmail.com>
2020-01-17 01:13:05 +0100

    feat: remove default style

--- src/index.js
+++ src/index.js
@@ -4,165 +4,159 @@ import exenv from "exenv";
 const isClientSide = exenv.canUseDOM;
 
 const eventListenerOptions = {
-  once: true,
-  capture: true,
-  passive: true
+    once: true,
+    capture: true,
+    passive: true
 };
 
-const defaultWrapperStyle = { display: "inline-block" };
-
 const getDisplayName = WrappedComponent => {
-  return WrappedComponent.displayName || WrappedComponent.name || "Component";
+    return WrappedComponent.displayName || WrappedComponent.name || "Component";
 };
 
 const withHydrationOnDemandServerSide = WrappedComponent => ({
-  wrapperProps,
-  ...props
+    wrapperProps,
+    ...props
 }) => (
-  <section
-    data-hydration-on-demand={true}
-    style={defaultWrapperStyle}
-    {...wrapperProps}
-  >
-    <WrappedComponent {...props} />
-  </section>
+    <section data-hydration-on-demand={true} {...wrapperProps}>
+        <WrappedComponent {...props} />
+    </section>
 );
 
 const withHydrationOnDemandClientSide = ({
-  disableFallback = false,
-  onBefore = Function.prototype,
-  on = []
+    disableFallback = false,
+    onBefore = Function.prototype,
+    on = []
 }) => WrappedComponent => {
-  const WithHydrationOnDemand = ({
-    forceHydration = false,
-    wrapperProps,
-    ...props
-  }) => {
-    const rootRef = useRef(null);
-    const cleanupFunctions = useRef([]);
-    const [isHydrated, setIsHydrated] = useState(false);
+    const WithHydrationOnDemand = ({
+        forceHydration = false,
+        wrapperProps,
+        ...props
+    }) => {
+        const rootRef = useRef(null);
+        const cleanupFunctions = useRef([]);
+        const [isHydrated, setIsHydrated] = useState(false);
 
-    const cleanUp = () => {
-      cleanupFunctions.current.forEach(fn => fn());
-      cleanupFunctions.current = [];
+        const cleanUp = () => {
+            cleanupFunctions.current.forEach(fn => fn());
+            cleanupFunctions.current = [];
+        };
+
+        const hydrate = async () => {
+            cleanUp();
+            if (isHydrated) return;
+
+            await onBefore();
+            setIsHydrated(true);
+        };
+
+        const initDOMEvent = (type, target = rootRef.current) => {
+            target.addEventListener(type, hydrate, eventListenerOptions);
+            cleanupFunctions.current.push(() => {
+                target.removeEventListener(type, hydrate, eventListenerOptions);
+            });
+        };
+
+        const initTimeout = (delay = 2000) => {
+            if (delay <= 0) return;
+
+            const timeout = setTimeout(hydrate, delay);
+            cleanupFunctions.current.push(() => clearTimeout(timeout));
+        };
+
+        const initIdleCallback = () => {
+            if (!("requestIdleCallback" in window)) {
+                initTimeout();
+                return;
+            }
+
+            const idleCallback = requestIdleCallback(
+                () => requestAnimationFrame(() => hydrate()),
+                { timeout: 500 }
+            );
+
+            if (!("cancelIdleCallback" in window)) return;
+
+            cleanupFunctions.current.push(() => {
+                cancelIdleCallback(idleCallback);
+            });
+        };
+
+        const initIntersectionObserver = options => {
+            if (!("IntersectionObserver" in window)) {
+                hydrate();
+                return;
+            }
+
+            new IntersectionObserver(([entry], observer) => {
+                if (!entry.isIntersecting || !(entry.intersectionRatio > 0))
+                    return;
+
+                observer.disconnect();
+                hydrate();
+            }, options).observe(rootRef.current);
+        };
+
+        const initEvent = (type, options) => {
+            switch (type) {
+                case "delay":
+                    initTimeout(options);
+                    break;
+                case "visible":
+                    initIntersectionObserver(options);
+                    break;
+                case "idle":
+                    initIdleCallback();
+                    break;
+                default:
+                    initDOMEvent(type, options);
+            }
+        };
+
+        useLayoutEffect(() => {
+            if (isHydrated) return;
+
+            const wasRenderedServerSide = !!rootRef.current.getAttribute(
+                "data-hydration-on-demand"
+            );
+            const shouldHydrate =
+                (!wasRenderedServerSide && !disableFallback) || forceHydration;
+            if (shouldHydrate) hydrate();
+        });
+
+        useEffect(() => {
+            if (isHydrated || forceHydration) return;
+            on.forEach(event =>
+                Array.isArray(event) ? initEvent(...event) : initEvent(event)
+            );
+        }, []);
+
+        if (!isHydrated)
+            return (
+                <section
+                    ref={rootRef}
+                    dangerouslySetInnerHTML={{ __html: "" }}
+                    suppressHydrationWarning
+                    {...wrapperProps}
+                />
+            );
+
+        return (
+            <section {...wrapperProps}>
+                <WrappedComponent {...props} />
+            </section>
+        );
     };
 
-    const hydrate = async () => {
-      cleanUp();
-      if (isHydrated) return;
+    WithHydrationOnDemand.displayName = `withHydrationOnDemand(${getDisplayName(
+        WrappedComponent
+    )})`;
 
-      await onBefore();
-      setIsHydrated(true);
-    };
-
-    const initDOMEvent = (type, target = rootRef.current) => {
-      target.addEventListener(type, hydrate, eventListenerOptions);
-      cleanupFunctions.current.push(() => {
-        target.removeEventListener(type, hydrate, eventListenerOptions);
-      });
-    };
-
-    const initTimeout = (delay = 2000) => {
-      if (delay <= 0) return;
-
-      const timeout = setTimeout(hydrate, delay);
-      cleanupFunctions.current.push(() => clearTimeout(timeout));
-    };
-
-    const initIdleCallback = () => {
-      if (!("requestIdleCallback" in window)) {
-        initTimeout();
-        return;
-      }
-
-      const idleCallback = requestIdleCallback(
-        () => requestAnimationFrame(() => hydrate()),
-        { timeout: 500 }
-      );
-
-      if (!("cancelIdleCallback" in window)) return;
-
-      cleanupFunctions.current.push(() => {
-        cancelIdleCallback(idleCallback);
-      });
-    };
-
-    const initIntersectionObserver = options => {
-      if (!("IntersectionObserver" in window)) {
-        hydrate();
-        return;
-      }
-
-      new IntersectionObserver(([entry], observer) => {
-        if (!entry.isIntersecting || !(entry.intersectionRatio > 0)) return;
-
-        observer.disconnect();
-        hydrate();
-      }, options).observe(rootRef.current);
-    };
-
-    const initEvent = (type, options) => {
-      switch (type) {
-        case "delay":
-          initTimeout(options);
-          break;
-        case "visible":
-          initIntersectionObserver(options);
-          break;
-        case "idle":
-          initIdleCallback();
-          break;
-        default:
-          initDOMEvent(type, options);
-      }
-    };
-
-    useLayoutEffect(() => {
-      if (isHydrated) return;
-
-      const wasRenderedServerSide = !!rootRef.current.getAttribute(
-        "data-hydration-on-demand"
-      );
-      const shouldHydrate =
-        (!wasRenderedServerSide && !disableFallback) || forceHydration;
-      if (shouldHydrate) hydrate();
-    });
-
-    useEffect(() => {
-      if (isHydrated || forceHydration) return;
-      on.forEach(event =>
-        Array.isArray(event) ? initEvent(...event) : initEvent(event)
-      );
-    }, []);
-
-    if (!isHydrated)
-      return (
-        <section
-          ref={rootRef}
-          dangerouslySetInnerHTML={{ __html: "" }}
-          suppressHydrationWarning
-          style={defaultWrapperStyle}
-          {...wrapperProps}
-        />
-      );
-
-    return (
-      <section style={defaultWrapperStyle} {...wrapperProps}>
-        <WrappedComponent {...props} />
-      </section>
-    );
-  };
-
-  WithHydrationOnDemand.displayName = `withHydrationOnDemand(${getDisplayName(
-    WrappedComponent
-  )})`;
-
-  return WithHydrationOnDemand;
+    return WithHydrationOnDemand;
 };
 const withHydrationOnDemand = (options = {}) => {
-  if (isClientSide) return withHydrationOnDemandClientSide(options);
+    if (isClientSide) return withHydrationOnDemandClientSide(options);
 
-  return withHydrationOnDemandServerSide;
+    return withHydrationOnDemandServerSide;
 };
 
 export default withHydrationOnDemand;
